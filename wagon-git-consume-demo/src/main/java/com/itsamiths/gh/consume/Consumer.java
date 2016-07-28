2181dcd8a90f4df31f0116fc08e674af9ebaef03
itsamiths <mkamithkumar@gmail.com>
2016-07-28 18:41:30 +0530

    wagon-git-consume-demo initial push

---
+++ wagon-git-consume-demo/src/main/java/com/itsamiths/gh/consume/Consumer.java
@@ -0,0 +1,11 @@
+package com.itsamiths.gh.consume;
+
+
+public class Consumer {
+
+	public static void main(String[] args) {
+		System.out.println("###### in Consumer");
+		com.itsamiths.gh.HelloWorld.callMe();
+	}
+
+}
---
+++ wagon-git-consume-demo/src/test/java/com/itsamiths/gh/consume/ConsumerTest.java
@@ -0,0 +1,11 @@
+package com.itsamiths.gh.consume;
+
+
+public class ConsumerTest {
+
+	public static void main(String[] args) {
+		System.out.println("###### in ConsumerTest");
+		com.itsamiths.gh.HelloWorld.callMe();
+	}
+
+}
