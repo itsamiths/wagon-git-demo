7d5779452e867f230f8a8a75ae1c5a7df502729e
itsamiths <mkamithkumar@gmail.com>
2016-07-28 18:35:22 +0530

    fixed issue with class file not included in created jar
    https://techgrafitti.wordpress.com/2015/03/02/maven-packaging-the-compiled-test-classes/

---
+++ wagon-git-release-demo/src/test/java/com/itsamiths/gh/HelloWorldTest.java
@@ -0,0 +1,7 @@
+package com.itsamiths.gh;
+
+public class HelloWorldTest {
+	public static void callMe() {
+		System.out.println("Hi there, you called me from com.itsamiths.gh.HelloWorldTest");
+	}
+}
